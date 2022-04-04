598f67eb7a71f6d5a6fd48a36907bb4d1676fd65
Tishev <85134635+Tishev@users.noreply.github.com>
2022-04-04 09:52:20 +0300

    lesson002

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { logDOM } from "@testing-library/react";
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import Message from "./components/Message";
+
+import common, { AUTHOR } from "./constants/common";
+import { element } from "prop-types";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+  const handleClick = () => {
+    if (value !== "") {
+      const newMessage = { text: value, author: AUTHOR.me };
+      setList([...list, newMessage]);
+    }
+  };
+  const handleInput = (event) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    let timerId;
+    if (list.length > 0 && list[list.length - 1].author !== AUTHOR.bot) {
+      const newMessage = { text: "Привет, как дела?", author: AUTHOR.bot };
+      timerId = setTimeout(() => {
+        setList([...list, newMessage]);
+      }, 1500);
+    }
+    return () => {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+    };
+  }, [list]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Cписок сообщений: </h4>
+        <br />
+        <br />
+        {list.map((element) => (
+          <div>
+            <p>{element.text}</p>
+            <sup>{element.author}</sup>
+          </div>
+        ))}
+
+        <div>
+          <input
+            onChange={handleInput}
+            value={value}
+            placeholder={"Введите текст"}
+          />
+          <button onClick={handleClick}>Добавить сообщение</button>
+        </div>
       </header>
     </div>
   );
